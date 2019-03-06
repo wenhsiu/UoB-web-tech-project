ed11b4c0640d301ea65b8591e5d1947b71407c8e
He-Ling <hilynn5@hotmail.com>
2019-03-06 10:58:24 +0000

    Add react module for login

---
+++ site/public/js/login.js
@@ -0,0 +1,36 @@
+class LoginForm extends React.Component{					
+    //const axios = require('axios');
+    state = {
+        account: "",
+        password: ""
+    }
+
+    handleClick = (e) => {
+        e.preventDefault()						
+        axios.post('/', this.state)
+    }
+    handleACChange = (e) => {
+        this.setState({account: e.target.value})						
+    }
+    handlePWChange = (e) => {
+        this.setState({password: e.target.value})						
+    }
+    render(){
+        return(							
+        <div id="login">
+            <form>
+                <input className="username" type="text" data-bind="textInput: filter, valueUpdate: 'afterkeydown'" placeholder="Username" />
+                <i className="fas fa-user username_icon "></i>
+            </form>
+            <form>
+                <input className="password" type="text" data-bind="textInput: filter, valueUpdate: 'afterkeydown'" placeholder="Password" />								
+                <i className="fas fa-lock password_icon"></i>	
+            </form>
+            <form>		
+                <button className='button'>Login</button>
+            </form>
+        </div>
+        )
+    }
+}
+ReactDOM.render(<LoginForm />, document.getElementById('myForm'));
