e0bea8a1f8ea57014bf745efebd6fa719d5984c5
He-Ling <hilynn5@hotmail.com>
2019-03-27 09:42:58 +0000

    uploaded image folder handling

--- site/public/js/server_registerRoute.js
+++ site/public/js/server_registerRoute.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./server_mysql');
+
+let fs = require('fs');
+let path = require('path');
 
 router.post('/UserRegister', (req, res) => {
     if(!req.body.account || req.body.account.length === 0){
@@ -9,14 +11,21 @@ router.post('/UserRegister', (req, res) => {
     }
 
     let item = req.body;   
-    var dbCmd = "INSERT INTO members VALUES('" + item.account + "', '" + item.password + "', '" + item.hint + "')";
-
+    let dbCmd = "INSERT INTO members VALUES('" + item.account + "', '" + item.password + "', '" + item.hint + "')";
+    
+    var jsonPath = path.join(__dirname, '..', 'img', item.account);
+    
     const connection = req.app.locals.connection;
     connection.query(dbCmd, (err, result) => {        
         if(err){res.status(400);}
         else {
-            console.log("record inserted");
-            res.status(200);
+            try{
+                fs.mkdirSync(jsonPath);
+                console.log("record inserted");
+                res.status(200);
+            }catch(err){
+                console.log(JSON.stringify(err));
+            }            
         }
     });
 });
