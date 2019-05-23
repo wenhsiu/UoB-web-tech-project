386823fa6efb6c28495e8ce964421dcdff2b6203
He-Ling <hilynn5@hotmail.com>
2019-05-23 16:59:42 +0100

    Added function for get details of individual item

--- site/public/js/server_cateItemRoute.js
+++ site/public/js/server_cateItemRoute.js
@@ -11,7 +11,21 @@ const picPath = path.join(__dirname, '..', 'uploadpics');
 
 router.get('/getItemsByCate', (req, res) => {
     let cateID = req.body.cateID;
-    let cmd = "SELECT * FROM items WHERE Category = ? ;";
+    let cmd = "SELECT ItemName, Details, Id FROM items WHERE Category = ? ;";
+
+    const connection = res.app.locals.connection;
+    connection.query(cmd, cateID, (err, rows) => {
+        if(err){
+            res.status(400).send().end();
+        }else{
+            res.send(rows);
+        }
+    })
+})
+
+router.get('/getOneItemById', (req, res) => {
+    let cateID = req.body.cateID;
+    let cmd = "SELECT * FROM items WHERE Id = ? ;";
 
     const connection = res.app.locals.connection;
     connection.query(cmd, cateID, (err, rows) => {
