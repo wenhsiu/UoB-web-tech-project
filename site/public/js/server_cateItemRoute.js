426afc8b700ddf361f09c4358911397736a4cd85
He-Ling <hilynn5@hotmail.com>
2019-05-23 17:07:44 +0100

    Added function to get latest items for HomePage

--- site/public/js/server_cateItemRoute.js
+++ site/public/js/server_cateItemRoute.js
@@ -2,12 +2,26 @@
 
 const express = require('express');
 const router = express.Router();
+const limit = 5;
 
 let fs = require('fs');
-
 let path = require('path');
 
 const picPath = path.join(__dirname, '..', 'uploadpics');
+
+router.get('/getLatestItems', (req, res) => {
+    
+    let cmd = "SELECT ItemName, Details, Id FROM items ORDER BY Id DESC LIMIT " + limit + " ;";
+
+    const connection = res.app.locals.connection;
+    connection.query(cmd, (err, rows) => {
+        if(err){
+            res.status(400).send().end();
+        }else{
+            res.send(rows);
+        }
+    })
+})
 
 router.get('/getItemsByCate', (req, res) => {
     let cateID = req.body.cateID;
@@ -24,11 +38,11 @@ router.get('/getItemsByCate', (req, res) => {
 })
 
 router.get('/getOneItemById', (req, res) => {
-    let cateID = req.body.cateID;
+    let ID = req.body.ID;
     let cmd = "SELECT * FROM items WHERE Id = ? ;";
 
     const connection = res.app.locals.connection;
-    connection.query(cmd, cateID, (err, rows) => {
+    connection.query(cmd, ID, (err, rows) => {
         if(err){
             res.status(400).send().end();
         }else{
