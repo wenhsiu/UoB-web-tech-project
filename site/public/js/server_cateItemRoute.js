d61a7c2330d60249dfe19e735cf23125c2f9e8bd
He-Ling <hilynn5@hotmail.com>
2019-05-24 00:33:05 +0100

    Fixed the picture path issue

--- site/public/js/server_cateItemRoute.js
+++ site/public/js/server_cateItemRoute.js
@@ -51,8 +51,9 @@ router.get('/getOneItemById', (req, res) => {
     })
 })
 
-router.get('/getImage', (req, res) => {    
-    res.sendFile(picPath + '/' + req.params.imgName);
+router.get('/getImage', (req, res) => { 
+    console.log(req.query);   
+    res.sendFile(picPath + '/' + req.query.imgName);
 })
 
 
