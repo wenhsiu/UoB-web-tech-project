2d7f57db5ccf32c85122fcc3736787f0786dfe60
Wen-Hsiu Hsu <alvina8418@gmail.com>
2019-05-24 12:22:51 +0100

    add url to link in MainPage.js

--- site/public/js/MainPage.js
+++ site/public/js/MainPage.js
@@ -12,7 +12,6 @@ class MainPage extends React.Component {
 
 		this.setNewItems = this.setNewItems.bind(this);
 		this.displayItems = this.displayItems.bind(this);
-		this.getItemsId = this.getItemsId.bind(this);
 
 		this.setNewItems();
 	}
@@ -26,35 +25,14 @@ class MainPage extends React.Component {
 		});
 	}
 
-	getItemsId() {
-		// console.log(this.state.itemId);
-		// console.log(this.state.latestItems[0]);
-		for(let i = 0; i < this.state.latestItems.length; i++) {
-			this.state.itemURL.push(`'item/'{this.state.latestItems[i].Id}`);
-
-			this.setState({
-				itemURL: this.state.itemURL
-			});
-		}
-	}
-
-
 	displayItems() {
-		// if(this.state.latestItems.length > 0) {
-		// 	console.log(this.state.latestItems[0].Id);
-		// }
-
-		// if(this.state.path.length > 0) {
-		// 	console.log(this.state.path[0]);
-		// }
-
 		return(
 			this.state.latestItems.map((element) => {
 				return(
 					<div className="card" key = {element.Id.toString()}>
 						<img src={"/getImage/" + element.Details} className="card-img-top" alt={element.ItemName}/>
 						<div className="card-body"> 
-							<a href={this.state.url} className="stretched-link">{element.ItemName}</a>
+							<a href={"item/" + element.Id} className="stretched-link">{element.ItemName}</a>
 						</div>
 					</div>
 				)
