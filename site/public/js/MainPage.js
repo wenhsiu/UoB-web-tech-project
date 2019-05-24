63cb600a88676172d77d7606f9831f53bb32387c
Wen-Hsiu Hsu <alvina8418@gmail.com>
2019-05-24 12:16:24 +0100

    mainpage edited

--- site/public/js/MainPage.js
+++ site/public/js/MainPage.js
@@ -1,18 +1,32 @@
 
 class MainPage extends React.Component {
+	// state = {
+	// 	latestItems:[],
+	// }
+
 	constructor(props) {
 		super(props);
 		this.state = {
 			latestItems:[],
-			path: null,
-			url: null,
+			path: [],
+			itemURL: [],
 			itemId: []
 		};
 
 		this.setNewItems = this.setNewItems.bind(this);
 		this.displayItems = this.displayItems.bind(this);
+		this.getItemsId = this.getItemsId.bind(this);
+		this.getImagePath = this.getImagePath.bind(this);
 
 		this.setNewItems();
+		// this.setState({itemId: this.state.latestItems.filter(this.getItemsId)})
+		// console.log(this.state.latestItems);
+		// this.getItemsId();
+		if(this.state.latestItems.length > 0) {
+			console.log(this.state.latestItems);
+			this.getItemsId();
+			// this.getImagePath();
+		}
 	}
 
     setNewItems() {
@@ -20,20 +34,61 @@ class MainPage extends React.Component {
 			if(res.data.length === 0){return;}
 			this.setState({
 				latestItems: res.data
-			});
-			
+			})
 		});
 	}
 
-	// getItemsId() {
-	// 	this.state.latestItems.map((element) => {
+	getItemsId() {
+		// console.log(this.state.itemId);
+		// console.log(this.state.latestItems[0]);
+		for(let i = 0; i < this.state.latestItems.length; i++) {
+			this.state.itemURL.push(`'item/'{this.state.latestItems[i].Id}`);
 
-	// 	})
-	// }
+			this.setState({
+				itemURL: this.state.itemURL
+			});
+		}
+	}
+
+	getImagePath() {
+		// this.state.latestItems.map((element) => {
+		// 	axios.get("/getImage", {
+		// 		params: {
+		// 			imgName: element.Details
+		// 		}
+		// 		}).then((res) => {
+		// 		if(res.data.length === 0){return;}
+		// 		this.setState({
+		// 			path: res.data
+		// 		})
+		// 	});
+		// });
+
+		for(let i = 0; i < this.state.latestItems.length; i++) {
+			axios.get("/getImage", {
+				params: {
+					imgName: this.state.latestItems.Details
+				}
+				}).then((res) => {
+				if(res.data.length === 0){return;}
+				this.setState({
+					path: res.data
+				})
+			});
+		}
+	}
 
 
 
 	displayItems() {
+		// if(this.state.latestItems.length > 0) {
+		// 	console.log(this.state.latestItems[0].Id);
+		// }
+
+		// if(this.state.path.length > 0) {
+		// 	console.log(this.state.path[0]);
+		// }
+
 		return(
 			this.state.latestItems.map((element) => {
 				
@@ -47,8 +102,6 @@ class MainPage extends React.Component {
 						path: res.data
 					})
 				});
-
-				// this.setState({url: 'item/:' + element.Id});
 
 
 				return(
