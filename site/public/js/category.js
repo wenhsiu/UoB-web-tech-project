62f2e7b0a5229190f0369d53f1787d38930217f9
Wen-Hsiu Hsu <alvina8418@gmail.com>
2019-05-26 12:38:57 +0100

    change banner.png

--- site/public/js/category.js
+++ site/public/js/category.js
@@ -7,16 +7,35 @@ class ItemsInCategory extends React.Component {
 
 		this.state = {
 			cate: array[2],
-			cateName:"",
+			cateName:[],
 			itemsList:[]
 		};
 
+		this.setCategory = this.setCategory.bind(this);
 		this.setItemsList = this.setItemsList.bind(this);
 		this.displayItems = this.displayItems.bind(this);
+		this.getCategory = this.getCategory.bind(this);
 
+		this.setCategory();
 		this.setItemsList();
+
 	}
 
+	setCategory() {
+    	axios.get("/getCateName/" + this.state.cate).then((res) => {
+			console.log(res.data);
+			if(res.data.length === 0){return;}
+			this.setState({
+				cateName: res.data
+			})		
+		});
+	}
+
+	getCategory() {
+		if(this.state.cateName.length > 0) {
+			return this.state.cateName[0].name
+		}
+	}
 
     setItemsList() {
     	axios.get("/getItemsByCate/" + this.state.cate).then((res) => {
@@ -47,7 +66,7 @@ class ItemsInCategory extends React.Component {
 		return (
 			<div>
 				<div>
-					<h2>{this.state.cate}</h2>
+					<h2>{this.getCategory()}</h2>
 					<div className="items">
 						{this.displayItems()}
 					</div>
