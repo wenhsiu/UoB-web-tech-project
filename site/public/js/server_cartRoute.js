3cba7a5d4a03b3dd56a3153a8049ec5d4282c148
He-Ling <hilynn5@hotmail.com>
2019-05-27 14:30:45 +0100

    Export module of cart route

--- site/public/js/server_cartRoute.js
+++ site/public/js/server_cartRoute.js
@@ -7,20 +7,6 @@ let fs = require('fs');
 let path = require('path');
 
 const picPath = path.join(__dirname, '..', 'uploadpics');
-/*
-router.get('/getLikedItemsByUsername/:username', (req, res) => {   
-    let cmd = "SELECT ItemId FROM likes WHERE Username = ? AND LikeItem = true ;" ;    
-
-    const connection = res.app.locals.connection;
-    connection.query(cmd, req.params.username, (err, rows) => {
-        if(err){
-            res.status(400).send().end();
-        }else{
-            res.send(rows);
-        }
-    })
-})
-*/
 
 router.get('/getItemsDetailedInfo/:username', (req, res) => {
     let cmd = "SELECT I.ItemName, I.Details, I.Id FROM items I INNER JOIN likes L ON I.Id = L.ItemId " +
@@ -39,3 +25,5 @@ router.get('/getItemsDetailedInfo/:username', (req, res) => {
 router.get('/getImage/:details', (req, res) => {       
     res.sendFile(picPath + "/" + req.params.details);
 })
+
+module.exports = router;
